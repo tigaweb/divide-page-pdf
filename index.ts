edf8f5d64ecb617f6fe06f3cf68c857d5c3969f7
tigaweb <nka.webtech.dev@gmail.com>
2025-03-06 18:53:00 +0900

    ベースの設計

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("test")
+import fs from 'fs';
+import { PDFDocument, rgb } from 'pdf-lib';
+
+async function devideDoublePagePDF(inputPath: string, outputPath: string) {
+  // 入力PDFの読み込み
+
+  // 新しいPDFの作成
+
+  // 入力PDFを分割して出力用のストリームに書き込む処理
+
+  // 新しいPDFの保存
+
+}
+
+devideDoublePagePDF('a.pdf', 'b.pdf').catch(console.error);
